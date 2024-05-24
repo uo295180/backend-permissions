b86ad88db965c93663a42bc81f33d93635f13976
orvizz <153991700+orvizz@users.noreply.github.com>
2024-05-24 11:51:17 +0200

    creación de un token JWT

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use("/permissions", routerPermissions)
 let routerUsers = require("./routers/routerUsers")
 app.use("/users", routerUsers)
 
+let routerLogin = require("./routers/routerLogin")
+app.use("/login", routerLogin)
+
 app.listen(port, () => {
     console.log("Servidor activo en "+port)
 })
--- routers/routerLogin.js
+++ routers/routerLogin.js
@@ -1,15 +1,23 @@
 const express = require("express")
 let routerLogin = express.Router()
 let users = require("../data/users")
+let jwt = require("jsonwebtoken")
 
 routerLogin.post("/", (req, res) => {
     let email = req.body.email
     let password = req.body.password
 
-    users.find(u => u.email == email && u.password == password)
+    let user = users.find(u => u.email == email && u.password == password)
     if( user == undefined ){
         return res.status(401).json({ error: "invalid username or password"})
     }
+
+    let apiKey = jwt.sign( {
+        email: user.email,
+        id: user.id,
+        time: Date.now()} , "secret")
+
+    res.json( { apiKey: apiKey })
 })
 
 module.exports = routerLogin
